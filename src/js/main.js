0258644a896b57cf67cf40d541ad3b22c0313431
oxmc <67136658+oxmc@users.noreply.github.com>
2021-04-10 03:53:50 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,59 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const notifier = require('node-notifier');
+const url = require('url');
+const path = require('path');
+
+var appdir = path.join(__dirname, '/src')
+var icondir = appdir + "/icons"
 
 let mainWindow;
+function updateavil() {
+notifier.notify ({
+               title: 'Update availible',
+               message: 'There is a new version of this app availible, click here to update.',
+               icon: icondir + "update.png"
+               sound: true,  // Only Notification Center or Windows Toasters
+               wait: true    // Wait with callback, until user action is taken 
+               against notification
+            
+            }, function (err, response) {
+               // Response is response from notification
+            });
+
+            notifier.on('click', function (notifierObject, options) {
+               console.log("User clicked on the notification, updating app.")
+            });
+
+            notifier.on('timeout', function (notifierObject, options) {
+               console.log("Notification timed out!")
+            });
+}
+let trayIcon = new Tray()
+
+         const trayMenuTemplate = [
+            {
+               label: 'UwUBot Status',
+               enabled: false
+            },
+            
+            {
+               label: 'Settings',
+               click: function () {
+                  console.log("Clicked on settings")
+               }
+            },
+            
+            {
+               label: 'Help',
+               click: function () {
+                  console.log("Clicked on Help")
+               }
+            }
+         ]
+         
+         let trayMenu = Menu.buildFromTemplate(trayMenuTemplate)
+         trayIcon.setContextMenu(trayMenu)
 
 function createWindow () {
   mainWindow = new BrowserWindow({
